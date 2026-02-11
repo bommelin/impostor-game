9d860342c3daad865ddf9962be046f6d2cd8dafe
Felix <felix.bommelin@gmail.com>
2026-02-11 12:21:42 +0100

    Fix offline font issue

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,11 @@
 /// <reference lib="webworker" />
 
 import { clientsClaim } from 'workbox-core'
+import { CacheableResponsePlugin } from 'workbox-cacheable-response'
+import { ExpirationPlugin } from 'workbox-expiration'
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies'
 
 self.skipWaiting()
 clientsClaim()
@@ -11,3 +14,37 @@ precacheAndRoute(self.__WB_MANIFEST)
 cleanupOutdatedCaches()
 
 registerRoute(new NavigationRoute(createHandlerBoundToURL('index.html')))
+
+// Cache Google Fonts stylesheets so font URLs remain available offline.
+registerRoute(
+  ({ url }) => url.origin === 'https://fonts.googleapis.com',
+  new StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 20,
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+      }),
+    ],
+  }),
+)
+
+// Cache Google Fonts binaries with a long-lived cache for offline use.
+registerRoute(
+  ({ url, request }) => url.origin === 'https://fonts.gstatic.com' && request.destination === 'font',
+  new CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 30,
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+      }),
+    ],
+  }),
+)
